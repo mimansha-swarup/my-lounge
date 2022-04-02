82df93ecccfae62b5baa6d64079e71228f34183a
mimansha-swarup <mimanshaswarup@gmail.com>
2022-04-02 17:53:14 +0530

    feat: asset

---
+++ src/Assets/index.js
@@ -0,0 +1,13 @@
+import banner from "./banner.svg"
+import BrandImage from "./BrandImage.svg"
+import lofi from "./lofi.svg"
+import anime from "./anime.svg"
+import bollywood from "./bollywood.svg"
+
+const categoriesImage={
+  lofi:lofi,
+  anime:anime,
+  bollywood:bollywood,
+}
+
+export {banner,BrandImage,categoriesImage}
