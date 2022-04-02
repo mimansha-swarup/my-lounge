fa0fc85ef91972e3e0aa45822990692f44d2a6c1
mimansha-swarup <mimanshaswarup@gmail.com>
2022-04-02 17:54:15 +0530

    feat: database update

--- src/backend/db/categories.js
+++ src/backend/db/categories.js
@@ -1,27 +1,32 @@
 import { v4 as uuid } from "uuid";
+import {categoriesImage} from "../../Assets/index"
 
 /**
  * Category Database can be added here.
  * You can add category of your wish with different attributes
  * */
 
+
 export const categories = [
   {
     _id: uuid(),
-    categoryName: "Computer Programming",
+    categoryName: "Lofi",
+    categoryImage: categoriesImage.lofi ,
     description:
-      "Computer programming is the process of designing and building an executable computer program to accomplish a specific computing",
+    "",
   },
   {
     _id: uuid(),
-    categoryName: "Frontend Development",
+    categoryName: "Bollywood",
+    categoryImage: categoriesImage.bollywood ,
     description:
-      "Front-end web development, also known as client-side development is the practice of producing HTML, CSS and JavaScript for a website or Web Application",
+    "",
   },
   {
     _id: uuid(),
-    categoryName: "Backend Development",
+    categoryName: "Anime",
+    categoryImage: categoriesImage.anime ,
     description:
-      "Backend Development is also known as server-side development. It is everything that the users don't see and contains behind-the-scenes activities that occur when performing any action on a website.",
+      "",
   },
 ];
