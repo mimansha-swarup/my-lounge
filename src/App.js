fc1569c1c3ff151aacb48aca185f6661bb83908f
mimansha-swarup <mimanshaswarup@gmail.com>
2022-04-03 10:37:48 +0530

    chore: cleaning

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import { Navbar, SideBar } from "./Components";
 import logo from "./logo.png";
-import { HomePage, VideoPage } from "./Page";
+import AllRoutes from "./Routes/AllRoutes";
+
 // import HomePage from "./Page";
 
 function App() {
@@ -12,11 +12,8 @@ function App() {
       
         <Navbar />
         <SideBar/>
-        {/* <HomePage/> */}
-        <Routes>
-          <Route path="/" element={<HomePage/>} />
-          <Route path="video" element={<VideoPage/>} />
-        </Routes>
+        <AllRoutes/>
+     
     </div>
   );
 }
---
+++ src/Helper/Api/ProviderWrapper.js
@@ -0,0 +1,7 @@
+import { ToastProvider } from "../../Context";
+
+const ProvideWrapper = ({ children }) => {
+  return <ToastProvider>{children}</ToastProvider>;
+};
+
+export default ProvideWrapper;
