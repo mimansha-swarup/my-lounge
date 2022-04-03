18758baf030af4c9c3b27672f869486e089d2db6
mimansha-swarup <mimanshaswarup@gmail.com>
2022-04-03 22:34:10 +0530

    new api path

--- src/Helper/Api/Api.js
+++ src/Helper/Api/Api.js
@@ -1,3 +1,5 @@
 export const categoriesApi = "/api/categories";
 export const signupPostApi = "/api/auth/signup";
 export const loginPostApi = "/api/auth/login";
+export const likesApi = "/api/user/likes";
+
