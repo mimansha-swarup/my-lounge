99858fad39412e101e1b65e0cfd75934c3c24cfc
mimansha-swarup <mimanshaswarup@gmail.com>
2022-04-05 13:26:12 +0530

    new Layout card

--- src/Page/Liked/LikedPage.js
+++ src/Page/Liked/LikedPage.js
@@ -1,6 +1,7 @@
 import "../Video/Video.css"
 import { useActivities, useLikes } from "../../Context";
-import {VideoCard} from "../../Components/index"
+import {HorizontalVideoCard, VideoCard} from "../../Components/index"
+import { likesApi } from "../../Helper/Api/Api";
 
 const LikedPage = () => {
   const {activitiesState} = useActivities()
@@ -10,11 +11,12 @@ const LikedPage = () => {
     
       <div className="video-cont">
       <h3 className="headline3">Liked Videos <span className="subtitle1 grey-text">({activitiesState.likes.length} Liked Videos)</span> </h3>
-        <div className="grid-x4">
-          {activitiesState.likes.map((video) => (
-            <VideoCard key={video._id} videoData={video} />
+        <div className="flex flex-column center gap-1">
+          {activitiesState.likes.map((video,index) => (
+            <HorizontalVideoCard key={video._id} index={index+1} videoData={video} dataKey={"likes"} api={likesApi} />
           ))}
         </div>
+       
       </div>
       
     </main>
