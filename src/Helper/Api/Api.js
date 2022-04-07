0e918c74f0da6480db5a0146703fe7f01a76d689
mimansha-swarup <mimanshaswarup@gmail.com>
2022-04-07 10:49:01 +0530

    new api path

--- src/Helper/Api/Api.js
+++ src/Helper/Api/Api.js
@@ -5,6 +5,7 @@ export const loginPostApi = "/api/auth/login";
 export const likesApi = "/api/user/likes";
 export const watchlaterApi = "/api/user/watchlater";
 export const historyApi = "/api/user/history";
+export const playlistApi = "/api/user/playlists";
 
 
-export const concatedApi = (api,id) => api+"/"+String(id);
+export const concatedApi= (...args) => args.join("/");
